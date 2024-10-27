082fa799c3a8784bfed12a95559873ee9a259abc
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-28 02:14:40 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "Script By Nexer1234 (ez) ",Icon = "rbxassetid://7733658504",Duration = 5})
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "Script By Nexer1234 (bro) ",Icon = "rbxassetid://7733658504",Duration = 5})
 
 wait(2)
 
